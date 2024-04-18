3fc8a84089e1b204b67a825aff1f7197f00f65d2
Ítalo Taveira <135046881+ItaloTaveira@users.noreply.github.com>
2024-04-17 23:46:14 -0300

    Update calculadora.js

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -1,38 +1,98 @@
 function Pontuação() {
-    let equipe = document.getElementById("equipe").value;
+    let equipe = Text(document.getElementById("equipe").value);
     let leite = Number(document.getElementById("leite").value);
     let sangue = Number(document.getElementById("sangue").value);
     let social = Number(document.getElementById("social").value);
     let apresentação = Number(document.getElementById("apresentação").value);
     let mascote = Number(document.getElementById("mascote").value);
     let quiz = Number(document.getElementById("quiz").value);
-    let extra = Number(document.getElementById("pontuaçãoextra").value);
-    let kit = Number(document.getElementById("kit").value);
-    let kitsextras = Number(document.getElementById("kitsextras").value);
-    let suplementosextras = Number(document.getElementById("suplementosextras").value);
+    let pontuaçãoextra = Number(document.getElementById("pontuaçãoextra").value);
+    let kits = Number(document.getElementById("kits").value);
+    let suplementos = Number(document.getElementById("suplementos").value)
+    let kitsextra = Number(document.getElementById("kitsextra").value);
+    let suplementosextra = Number(document.getElementById("suplementosextra").value);
     let arroz05 = Number(document.getElementById("arroz05").value);
     let arroz01 = Number(document.getElementById("arroz01").value);
     let feijão02 = Number(document.getElementById("feijão02").value);
     let feijão01 = Number(document.getElementById("feijão01").value);
     let macarrão = Number(document.getElementById("macarrão").value);
-    let oleo = Number(document.getElementById("oleo").value);
+    let oleo = Number(document.getElementById("oleo").value);   
 
-    let resultado = 0;
-    if (equipe === "1") { 
-        let l = leite * 2;
-        let s = sangue * 20;
-        let k = (kit * 100) / 91;
-        let ke = kitsextras * 30;
-        let se = suplementosextras * 15;
-        let a05 = arroz05 * 5;
-        let a01 = arroz01 * 1;
-        let f02 = feijão02 * 2;
-        let f01 = feijão01 * 1;
-        let m = macarrão * 0.5;
-        let o = oleo * 1;
-
-        resultado = l + s + social + apresentação + mascote + quiz + extra + k + ke + se + a05 + a01 + f02 + f01 + m + o;
+    switch (equipe.toLowerCase()) {
+        case "amarela":
+            equipeNome
+            let l = leite * 2
+            let s = sangue * 20;
+            let k = (kits * 100) / 91
+            let ke = kitsextra * 30
+            let su = (suplementos * 100) / 41
+            let se = suplementosextra * 15
+            let a05 = arroz05 * 5
+            let a01 = arroz01 * 1
+            let f02 = feijão02 * 2
+            let f01 = feijão01 * 1
+            let m = macarrão * 0.5
+            let o = oleo * 1
+            resultado = (l + s + social + apresentação + mascote + quiz + pontuaçãoextra + k + su + ke + se + a05 + a01 + f02 + f01 + m + o)
+            document.getElementById("resultado").textContent
+            break;
+        case "azul":
+            equipeNome = "Equipe Azul";
+            let l2 = leite * 2
+            let s2 = sangue * 20;
+            let k2 = (kits * 100) / 92
+            let ke2 = kitsextra * 30
+            let su2 = (suplementos * 100) / 41
+            let se2 = suplementosextra * 15
+            let a052 = arroz05 * 5
+            let a012 = arroz01 * 1
+            let f022 = feijão02 * 2
+            let f012 = feijão01 * 1
+            let m2 = macarrão * 0.5
+            let o2 = oleo * 1
+            resultado = (l2 + s2 + social + apresentação + mascote + quiz + pontuaçãoextra + k2 + su2 + ke2 + se2 + a052 + a012 + f022 + f012 + m2 + o2)
+            document.getElementById("resultado").textContent
+            break;
+        case "roxa":
+            equipeNome = "Equipe Roxa";
+            equipeNome = "Equipe Azul";
+            let l3 = leite * 2
+            let s3 = sangue * 20;
+            let k3 = (kits * 100) / 92
+            let ke3 = kitsextra * 30
+            let su3 = (suplementos * 100) / 41
+            let se3 = suplementosextra * 15
+            let a053 = arroz05 * 5
+            let a013 = arroz01 * 1
+            let f023 = feijão02 * 2
+            let f013 = feijão01 * 1
+            let m3 = macarrão * 0.5
+            let o3 = oleo * 1
+            resultado = (l3 + s3 + social + apresentação + mascote + quiz + pontuaçãoextra + k3 + su3 + ke3 + se3 + a053 + a013 + f023 + f013 + m3 + o3)
+            document.getElementById("resultado").textContent
+            break;
+        case "vermelho":
+            equipeNome = "Equipe Vermelha";
+            equipeNome = "Equipe Azul";
+            let l4 = leite * 2
+            let s4 = sangue * 20;
+            let k4 = (kits * 100) / 92
+            let ke4 = kitsextra * 30
+            let su4 = (suplementos * 100) / 41
+            let se4 = suplementosextra * 15
+            let a054 = arroz05 * 5
+            let a014 = arroz01 * 1
+            let f024 = feijão02 * 2
+            let f014 = feijão01 * 1
+            let m4 = macarrão * 0.5
+            let o4 = oleo * 1
+            resultado = (l4 + s4 + social + apresentação + mascote + quiz + pontuaçãoextra + k4 + su4 + ke4 + se4 + a054 + a014 + f024 + f014 + m4 + o4)
+            document.getElementById("resultado").textContent
+            break;
+        default:
+            document.getElementById("resultado").textContent = "Equipe desconhecida";
+            return;
     }
 
-    document.getElementById("resultado").textContent = resultado.toString();
+    
 }
